6ae23defdc5e7f4e3892407f2fb59e63cc434b74
David Markusfeld <david@markusfeld.at>
2016-01-16 15:17:11 +0100

    selenium login Verein/Veranstalter Username Password

--- src/test/java/at/ac/tuwien/inso/swtesten/lab/LabSeleniumHelper.java
+++ src/test/java/at/ac/tuwien/inso/swtesten/lab/LabSeleniumHelper.java
@@ -2,6 +2,9 @@ package at.ac.tuwien.inso.swtesten.lab;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 import at.ac.tuwien.inso.swtesten.util.SeleniumWebDriver;
@@ -16,7 +19,43 @@ public class LabSeleniumHelper {
 		baseUrl = "http://accreditation.qtixx.com/test/";
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
+	
+	public void loginVereinWithUsernameAndPassword(String username, String password) throws Exception {
+	    driver.get(baseUrl + "/test/set-online/index.php");
+	    driver.findElement(By.id("login_username")).clear();
+	    driver.findElement(By.id("login_username")).sendKeys(username);
+	    driver.findElement(By.id("login_passwd")).clear();
+	    driver.findElement(By.id("login_passwd")).sendKeys(password);
+	    driver.findElement(By.id("login")).click();
+	  }
+	
+	public void loginVeranstalterWithUsernameAndPassword(String username, String password) throws Exception {
+	    driver.get(baseUrl + "/test/set-online/index.php");
+	    driver.findElement(By.id("admin_login_username")).clear();
+	    driver.findElement(By.id("admin_login_username")).sendKeys(username);
+	    driver.findElement(By.id("admin_login_passwd")).clear();
+	    driver.findElement(By.id("admin_login_passwd")).sendKeys(password);
+	    driver.findElement(By.id("login2")).click();
+	  }
 
+	private boolean isElementPresent(By by) {
+	    try {
+	      driver.findElement(by);
+	      return true;
+	    } catch (NoSuchElementException e) {
+	      return false;
+	    }
+	  }
+
+	  private boolean isAlertPresent() {
+	    try {
+	      driver.switchTo().alert();
+	      return true;
+	    } catch (NoAlertPresentException e) {
+	      return false;
+	    }
+	  }
+	
 	public void shutDown() {
 		SeleniumWebDriver.closeDriver();
 	}
